f6c7a3109156336af5b8486e61237f5a932d0c50
trescejas <trescejas@sutura.local>
2026-05-16 18:59:52 +0200

    Snapshot WordPress-rendered Sutura site

--- assets/js/site.js
+++
@@ -1,9 +0,0 @@
-
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav-links');
-if (navToggle && navLinks) {
-  navToggle.addEventListener('click', () => {
-    const open = navLinks.classList.toggle('is-open');
-    navToggle.setAttribute('aria-expanded', String(open));
-  });
-}
